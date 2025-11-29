6c0ea8663dcde3625eb721f06287bcfe600dadcc
RaulRuizBolivar <raul.ruiz.bolivar@gmail.com>
2025-11-29 22:38:16 +0100

    Hello World Exercises

---
+++ HelloWorldExercises.java
@@ -0,0 +1,55 @@
+public class HelloWorldExercises {
+
+  public static void main(final String[] args) {
+
+    // 1
+    System.out.println("Raul");
+
+    // 2
+    System.out.println("Hola");
+    System.out.println("Mundo");
+
+    // 4
+    /*
+     * Este es un comentario de varias lineas
+     *
+     */
+
+    // 5
+    System.out.println("26 , Verde, Camarma");
+
+    // 6
+    System.err.println("Este es un error");
+
+    // 7
+    System.out.println("Utiliza");
+    System.out.println("varios");
+    System.out.println("println");
+    System.out.println("para");
+    System.out.println("imprimir");
+    System.out.println("una");
+    System.out.println("frase");
+
+    // 8
+    System.out.println(
+        "────────────────████\n───────────────█░░███\n───────────────█░░████\n────────────────███▒██─────████████\n──────████████─────█▒█──████▒▒▒▒▒▒████\n────███▒▒▒▒▒▒████████████░░████▒▒▒▒▒███\n──██▒▒▒▒░▒▒████░░██░░░░██░░░░░█▒▒▒▒▒▒▒███\n─██▒▒░░░░▒██░░░░░█▒░░░░░██▒░░░░░░░▒▒▒▒▒▒█\n██▒░░░░░▒░░░░░░░░░▒░░░░░░░▒▒░░░░░░░▒▒▒▒▒██\n█░░░░░░▒░░░██░░░░░░░░░░░░░██░░░░░░░░▒▒▒▒▒█\n█░░░░░░░░█▒▒███░░░░░░░░░█▒▒███░░░░░░░▒▒▒▒█\n█░░░░░░░████████░░░░░░░████████░░░░░░▒▒▒▒█\n█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒█\n██░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█░▒▒▒▒█\n─█░░░░██░█░░░░░░░░░░░░░░░░░░░░░░░███▒▒▒▒▒█\n─█▒▒░░░░█████░░░█░░░░██░░░██░░████░▒▒▒▒▒▒█\n─██▒▒░░░░░█████████████████████░░░▒▒▒▒▒▒██\n──██▒▒▒▒░░░░░██░░░███░░░██░░░█░░░▒▒▒▒▒▒██\n───███▒▒▒░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒█████\n─────███▒▒▒▒▒▒░░░░░░░░░░░░░▒▒▒▒▒▒███\n────────██████████████████████████");
+
+  }
+}
+
+// 1. Imprime un mensaje que diga tu nombre
+//
+// 2. Imprime dos lineas: "Hola" y luego "Mundo" con un solo println
+//
+// 3. Añade un comentario sobre lo que hacer cada linea del programa
+//
+// 4. Crea un comentario en varias lineas
+//
+// 5. Imprime tu edad, tu color favorito y tu ciudad
+//
+// 6. Explora los diferentes System.XXX.println();
+//
+// 7. Utiliza varios println para imprimir una frase
+//
+// 8. Imprime un diseño ASCII
+//
